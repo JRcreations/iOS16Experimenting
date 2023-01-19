c50ad4d034a2abbd656cfbededa547c818b3f3d9
John Reichel <jreichel447@gmail.com>
2023-01-19 09:11:13 -0600

    Initial Commit

---
+++ iOS16/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS16
+//
+//  Created by John Reichel on 1/19/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS16/iOS16App.swift
@@ -0,0 +1,17 @@
+//
+//  iOS16App.swift
+//  iOS16
+//
+//  Created by John Reichel on 1/19/23.
+//
+
+import SwiftUI
+
+@main
+struct iOS16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
